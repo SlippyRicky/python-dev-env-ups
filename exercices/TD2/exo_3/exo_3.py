8f1fa431ee98ff3d2d3ad162fe43fbfa40db0521
SlippyRicky <122894088+SlippyRicky@users.noreply.github.com>
2025-10-09 11:09:41 +0200

    Update exo_3.py
    unwhack

--- exercices/TD2/exo_3/exo_3.py
+++ exercices/TD2/exo_3/exo_3.py
@@ -1,24 +1,25 @@
-reponse = int(input("Voulez vous 1) Convertir d'annÃ©es humaines ou 2) cannines? 1 ou 2:"))
+def conv_homme_chien(annees_humaines):
+    if annees_humaines <= 18:
+        return annees_humaines / 9
+    else:
+        return 2 + (annees_humaines - 18) / 6
 
+def conv_chien_homme(annees_canines):
+    if annees_canines <= 2:
+        return annees_canines * 9
+    else:
+        return 18 + (annees_canines - 2) * 6
 
-annees_humaines = int(input("Entrez l'Ã¢ge en annÃ©es humaines : "))
-annees_canines = int(input("Entrez l'Ã¢ge du chien en annÃ©es : "))
+# Demande à l'utilisateur ce qu'il veut convertir
+reponse = int(input("Voulez-vous : 1) Convertir d'années humaines en années canines, ou 2) d'années canines en années humaines ? (1 ou 2) : "))
 
 if reponse == 1:
-    conv_homme_chien(annees_humaines, annees_canines)
-    print(f"{annees_humaines} annÃ©e(s) humaine(s) Ã©quivalent Ã  {annees_canines:.1f} annÃ©e(s) canine(s).")
-    else:
-        conv_chien_homme(annees_canines, annees_humaines)
-        print(f"{annees_canines} annÃ©e(s) canine(s) Ã©quivalent Ã  {annees_humaines} annÃ©e(s) humaine(s).")
-
-def conv_homme_chien(annees_humaines, annees_canines):
-    if annees_humaines <= 18:
-        annees_canines = annees_humaines / 9
-    else:
-        annees_canines = 2 + (annees_humaines - 18) / 6
-        
-def conv_chien_homme(annees_canines, annees_humaines):
-    if annees_canines <= 2:
-        annees_humaines = annees_canines * 9
-    else:
-        annees_humaines = 2 * 9 + (annees_canines - 2) * 6
+    annees_humaines = int(input("Entrez l'âge en années humaines : "))
+    annees_canines = conv_homme_chien(annees_humaines)
+    print(f"{annees_humaines} année(s) humaine(s) équivalent à {annees_canines:.1f} année(s) canine(s).")
+elif reponse == 2:
+    annees_canines = int(input("Entrez l'âge du chien en années : "))
+    annees_humaines = conv_chien_homme(annees_canines)
+    print(f"{annees_canines} année(s) canine(s) équivalent à {annees_humaines} année(s) humaine(s).")
+else:
+    print("Réponse invalide. Veuillez entrer 1 ou 2.")
