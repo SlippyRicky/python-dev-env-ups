e40c8789d7aa079593914f35caaef900c86309b7
SlippyRicky <emeric.mellet@outlook.com>
2025-11-06 09:24:45 +0100

    Creation of TD4 file structure

--- Bash/make/make_files.sh
+++ Bash/make/make_files.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-for dir in exo_{1..8}; do
+for dir in exo_{1..3}; do
+  x=0
     if [ -d "$dir" ]; then
+        x=+1
         # Navigate into the directory
         cd "$dir"
         # Create a Python file with the same name as the directory
-        touch "${dir}.py"
+        touch "${dir}_$x.py"
+        touch "${dir}+$x.py"
+        touch "${dir}++$x.py"
         # Navigate back to the original directory
         cd ..
     fi
