600204650e98ac464d8cec12f0c233dc76ad9e53
SlippyRicky <emeric.mellet@outlook.com>
2025-11-13 10:29:17 +0100

    TD4

--- exercices/TD4/exo_3/exo+3.py
+++ exercices/TD4/exo_3/exo+3.py
@@ -2,25 +2,18 @@ import numpy as np
 
 # Définir les dimensions de la matrice
 m = 3  # nombre de lignes
-n = 4  # nombre de colonnes
-
-# Créer la matrice A où chaque élément a_ij = i * j
+n = 4  
 A = np.array([[i * j for j in range(1, n + 1)] for i in range(1, m + 1)])
 
-# Afficher la matrice A
 print("Matrice A :")
 print(A)
 
-# Extraire les vecteurs v et w correspondant à la première et dernière colonne
-v = A[:, 0]  # Première colonne
-w = A[:, -1]  # Dernière colonne
+v = A[:, 0]
+w = A[:, -1]
 
-# Afficher les vecteurs v et w
 print("\nVecteur v (première colonne) :", v)
 print("Vecteur w (dernière colonne) :", w)
 
-# Calculer le produit scalaire entre v et w
 produit_scalaire = np.dot(v, w)
 
-# Afficher le produit scalaire
 print("\nProduit scalaire entre v et w :", produit_scalaire)
